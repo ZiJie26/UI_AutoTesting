defe2d57fe5b24c054537797f7f7a334e1d006a4
ZiJie <zijie26@outlook.com>
2024-08-19 15:11:17 +0800

    多套件运行测试

---
+++ suites/suit2.py
@@ -0,0 +1,13 @@
+# 导入你希望包括在测试套件中的测试用例
+from cases.testFeature.test_test1 import Test1
+from cases.testFeature.test_test2 import Test2
+
+
+# 创建一个测试套件
+class Suite:
+    def run_tests(self):
+        test_cases = [Test2]  # 将测试用例添加到测试套件中
+
+        for test_case in test_cases:
+            # 直接调用测试用例
+            test_case()
