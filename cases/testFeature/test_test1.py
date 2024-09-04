01b82d2e47fbb62b54d097a301db62e82efbe8b1
ZiJie <zijie26@outlook.com>
2024-09-04 22:23:45 +0800

    update

--- cases/testFeature/test_test1.py
+++ cases/testFeature/test_test1.py
@@ -6,31 +6,38 @@ from page.base_page import Base
 
 class Test1(Base):
     @pytest.mark.demo
-    def test_1(self, db_connection):
-        # 获取数据库连接
-        connection = db_connection
+    def test_1(self):
+        self.driver.get("https://www.qq.com/")
 
-        # 执行查询
-        try:
-            with connection.cursor() as cursor:
-                query = """
-                SELECT
-                    id, 
-                    `code`, 
-                    phone_number
-                FROM
-                    sys_sms_record
-                ORDER BY id DESC
-                LIMIT 1
-                """
-                cursor.execute(query)
-                result = cursor.fetchone()
+    # def test_1(self, db_connection):
+    #     """
+    #     这个方法为数据库查询的模板代码
+    #     需要在config\db_config.json里填上正确的数据库信息才能用哦~
+    #     """
+    #     # 获取数据库连接
+    #     connection = db_connection
 
-                if result:
-                    print(
-                        f"Latest record - ID: {result[0]}, Code: {result[1]}, Phone Number: {result[2]}"
-                    )
-                else:
-                    print("No records found.")
-        except pymysql.MySQLError as e:
-            print(f"Error executing query: {e}")
+    #     # 执行查询
+    #     try:
+    #         with connection.cursor() as cursor:
+    #             query = """
+    #             SELECT
+    #                 id,
+    #                 `code`,
+    #                 phone_number
+    #             FROM
+    #                 sys_sms_record
+    #             ORDER BY id DESC
+    #             LIMIT 1
+    #             """
+    #             cursor.execute(query)
+    #             result = cursor.fetchone()
+
+    #             if result:
+    #                 print(
+    #                     f"Latest record - ID: {result[0]}, Code: {result[1]}, Phone Number: {result[2]}"
+    #                 )
+    #             else:
+    #                 print("No records found.")
+    #     except pymysql.MySQLError as e:
+    #         print(f"Error executing query: {e}")
